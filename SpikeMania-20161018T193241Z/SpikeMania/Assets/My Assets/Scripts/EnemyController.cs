bec4b58fc98777d989018fee64c6b64f38945b6e
Andrew MacMillan <andrewmacmilan1@hotmail.com>
2016-10-18 18:38:57 -0400

    no message

--- SpikeMania-20161018T193241Z/SpikeMania/Assets/My Assets/Scripts/EnemyController.cs
+++ SpikeMania-20161018T193241Z/SpikeMania/Assets/My Assets/Scripts/EnemyController.cs
@@ -6,15 +6,39 @@ public class EnemyController : MonoBehaviour {
 
     private int takeDmg;
     private int myHealth = 100;
+
+    //[SerializeField]
+    public Transform Character;
+    [SerializeField]
+    private float distanceToTarget = 2;
+    bool moving = true;
 	// Use this for initialization
+
+
 	void Start () {
-	
+        //GameObject.Find("Character").transform.position = Character.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //DistanceCheck();
+        Movement();
 	}
+    void DistanceCheck()
+    {
+        if (Vector2.Distance(transform.position,Character.position) <= distanceToTarget)
+        {
+            moving = false;
+        }
+    }
+    void Movement()
+    {
+        while(moving)
+        {
+            transform.Translate(Vector2.left * 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Bullet")
